a9f2104670758e02dcb4872b10b91cd3e3e1cc86
JerBartels <jer@iamjer.be>
2016-11-20 17:56:19 +0100

    added first interactivity
    some buttons and some code!

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //app to order coffee
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //method called when order button is clicked
+    public void submitOrder(View view){
+        display(2);
+        displayPrice(2*10);
+    }
+
+    //method that display the given quantity on the screen
+    public void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText("€" + number);
     }
 }
